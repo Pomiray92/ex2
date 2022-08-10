c3385d825d3c1a1dae4191590c69ccecf7fe1e45
writehereyourname <writehereyouremail>
2022-08-10 15:48:08 +0200

    New

---
+++ exercise.py
@@ -0,0 +1,67 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+#task1ex2
+
+from colorsys import rgb_to_hls, rgb_to_hsv
+'''
+
+print(int(234))
+print(float(43.12))
+print(complex((3+2j)))
+print(str("Hello"))
+print(bool(True))
+
+#task2ex2
+
+a = 123
+print(a, "is type of", int)
+
+b = 43.23
+print(b, "is type of", float)
+
+c = (4 - 1j)
+print(c, "is type of", complex) 
+
+d = ("how are you")
+print(d, "is type of", str)
+
+e = True
+print(e, "is type of", bool)
+
+#task3ex2
+
+city = "Fürth"
+print(isinstance(city, str))
+print(isinstance(city, int))
+print(isinstance(city, float))
+print(isinstance(city, bool))
+print(isinstance(city, complex))
+'''
+#task4ex2
+
+ab = 123
+ac = int
+print("is", ab, "an instance of int?:", isinstance(ab, ac))
+
+ba = 43.23
+bb = bool
+print("is", ba, "instance of bool?:", isinstance(ba, bb))
+
+ca = (4-1j)
+cb = complex
+print("is", ca, "instance of complex?:", isinstance(ca, cb))
+
+da = True
+db = bool
+print("is", da, "an instance of bool?:", isinstance(da, db))
+
+ea = ("how are you")
+eb = float
+print("is", ea, "an instance of float?:", isinstance(ea, eb))
+
+
+
+#task5ex2
+
